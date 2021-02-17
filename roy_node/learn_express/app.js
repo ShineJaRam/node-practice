7970b55d0e77d2c45a576a5cf0a2ae6440e94e23
수파자 <supaja@supajaui-MacBook-Pro.local>
2021-02-17 18:36:43 +0900

    21/02/17

--- roy_node/learn_express/app.js
+++ roy_node/learn_express/app.js
@@ -1,17 +1,52 @@
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const multer = require('multer')
+const fs = require('fs')
 const app = express()
+
+const indexRouter = require('./routes')
+const userRouter = require('./routes/user')
 
 app.set('port', process.env.PORT || 3000)
 
+app.set('view engine', 'html')
+
+app.use(morgan('dev'))
+app.use(cookieParser(process.env.COOKIE_SECRET))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+// app.use('요청 경로', express.static('실제 경로'))
+app.use(session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true
+    },
+    // name: 'connect,sid'
+}))
+
+app.use('/', indexRouter)
+app.use('/user', userRouter)
+
+app.use('/', (req, res, next) => {
+    if (req.session.id) {
+        express.static(__dirname, 'public')(req, res, next)
+    } else {
+        next()
+    }
+}) 
+
 app.use((req, res, next) => {
-    console.log('모든 요청에 실행하고 싶어요1')
-    next()
-}, (req, res, next) => {
-    throw new Error('에러가 났어요')
+    req.data = "로이의 비밀번호?!"
 })
 
 app.get('/', (req, res) => {
+    req.data
     res.sendFile(path.join(__dirname, './index.html'))
 })
 
@@ -27,13 +62,13 @@ app.get('/about', (req, res) => {
     res.send('hello express')
 })
 
+
 app.use((req, res, next) => {
-    res.send('404입니다.')
+    res.status(200).send('404입니다.')
 })
 
 app.use((error, request, response, next) => {
-    console.error(error)
-    response.send('에러났지롱. 근데 안알랴줌')
+    response.status(200).send('에러났지롱. 근데 안알랴줌')
 })
 
 app.listen(app.get('port'), () => {
